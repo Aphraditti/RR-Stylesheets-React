8379ec146c6369fdce9b5d658488a441753b1c45
Aphra Hiat <aphrahiat@Aphras-MBP.lan>
2023-04-20 12:36:59 -0700

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Container from './components/Container'
+import Menu from './components/Menu'
+import Aside from './components/Aside'
+
+const menu = {
+  backgroundColor: '#CCFF33'
+}
+const menuItem = {
+  backgroundColor: 'teal',
+  color: 'white',
+  fontSize: '2rem'
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Container>
+        <h2>This is the container child</h2>
+        <Menu menuStyle={menu} menuItem={ menuItem } />
+        <Aside />
+      </Container>
     </div>
   );
 }
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,3 @@
+export default function MenuItem({ menuItem, Label }) {
+    return <li style={ menuItem }>{ Label }</li>
+}
